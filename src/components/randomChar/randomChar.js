f9a9dec970209afcc14266bf7abb285e9b45242f
teddycomrad <teddycomrad21@gmail.com>
2021-09-13 14:48:34 +0300

    randomChar refactoring to React Hooks

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,64 +1,119 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import './randomChar.css';
 import gotService from '../../services/gotService';
 import Spinner from '../spinner';
 import ErrorMessage from '../errorMessage';
+import GotService from '../../services/gotService';
 
-export default class RandomChar extends Component {
 
-    gotService = new gotService();
-    state = {
-        char: {},
-        loading: true,
-        error: false
+// =============================================== классовый способ
+
+// export default class RandomChar extends Component {
+
+//     gotService = new gotService();
+//     state = {
+//         char: {},
+//         loading: true,
+//         error: false
+//     }
+
+//     componentDidMount() {
+//         this.updateChar();
+//         this.timerId = setInterval(this.updateChar, 15000);
+//     }
+
+//     componentWillUnmount(){
+//         clearInterval(this.timerId);
+//     }
+
+//     onCharLoaded = (char) => {
+//         this.setState({
+//             char,
+//             loading: false
+//         })
+//     }
+
+//     onError = (err) => {
+//         this.setState({
+//             error: true,
+//             loading: false
+//         })
+//     }
+
+//     updateChar = () => {
+//         const id = Math.floor(Math.random()*140 + 25); //25-140
+//         this.gotService.getCharacter(id)
+//             .then(this.onCharLoaded)
+//             .catch(this.onError);
+//     }
+
+//     render() {
+//         const { char, loading, error } = this.state;
+
+//         const errorMessage = error ? <ErrorMessage/> : null;
+//         const spinner = loading ? <Spinner/> : null;
+//         const content = !(loading || error) ? <View char={char}/> : null;
+
+//         return (
+//             <div className="random-block rounded">
+//                 {errorMessage}
+//                 {spinner}
+//                 {content}
+//             </div>
+//         );
+//     }
+// }
+
+
+// ======================================================= на хуках!
+
+
+
+export default function RandomChar() {
+    let gotService = new GotService();
+
+    const [char, setChar] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+    function onError(err) {
+        setError(true);
+        setLoading(false);
     }
 
-    componentDidMount() {
-        this.updateChar();
-        this.timerId = setInterval(this.updateChar, 15000);
-    }
-
-    componentWillUnmount(){
-        clearInterval(this.timerId);
-    }
-
-    onCharLoaded = (char) => {
-        this.setState({
-            char,
-            loading: false
-        })
-    }
-
-    onError = (err) => {
-        this.setState({
-            error: true,
-            loading: false
-        })
-    }
-
-    updateChar = () => {
+    function updateChar() {
         const id = Math.floor(Math.random()*140 + 25); //25-140
-        this.gotService.getCharacter(id)
-            .then(this.onCharLoaded)
-            .catch(this.onError);
+        gotService.getCharacter(id)
+            .then((pers => {
+                setChar(pers);
+                setLoading(false);
+            }))
+            .catch(onError);
     }
 
-    render() {
-        const { char, loading, error } = this.state;
+    useEffect(() => {
+        updateChar();
+        let timerId = setInterval(updateChar, 15000);
 
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const spinner = loading ? <Spinner/> : null;
-        const content = !(loading || error) ? <View char={char}/> : null;
+        return () => {
+            clearInterval(timerId);
+        }
+    }, []);
 
-        return (
+
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = !(loading || error) ? <View char={char}/> : null;
+
+    return (
             <div className="random-block rounded">
-                {errorMessage}
-                {spinner}
-                {content}
+                 {errorMessage}
+                 {spinner}
+                 {content}
             </div>
-        );
-    }
+    )
 }
+
 const View = ({char}) => {
     const {name, gender, born, died, culture} = char;
     return (
--- src/index.js
+++ src/index.js
@@ -4,56 +4,27 @@ import App from './components/app';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
-
-// class App extends Component {
-//     state = {
-//         num: 0
-//     }
-
-//     increment = () => {
-//         this.setState({
-//             num: this.state.num + 1
-//         })
-//     }
-
-//     decrement = () => {
-//         this.setState({
-//             num: this.state.num - 1
-//         })
-//     }
-
-//     render() {
-//         const {num} = this.state;
-//         return (
-//             <div>
-//                 <h1>{num}</h1>
-//                 <button
-//                     onClick={this.increment}>Increment
-//                 </button>
-//                 <button
-//                     onClick={this.decrement}>Decrement
-//                 </button>
-//             </div>
-//         )
-//     }
-// }
-
 // function App() {
-//     let [likes, setCount] = useState(0);
 
-//     function increment() {
-//         setCount(likes+1);
+//     const [count, setCount] = useState(0);
+
+//     function increase() {
+//         return setCount(prev => prev + 1);
 //     }
 
-//     function decrement() {
-//         setCount(likes-1);
+//     function decrease() {
+//         return setCount(prev => prev - 1);
 //     }
+
+//     useEffect(() => {
+//         console.log(`Вы нажали ${count} раз`);
+//     })
 
 //     return (
 //         <div>
-//             <h1>{likes}</h1>
-//             <button onClick={increment}>Increment</button>
-//             <button onClick={decrement}>Decrement</button>
+//             <h1>{count}</h1>
+//             <button onClick={increase}>Increase</button>
+//             <button onClick={decrease}>Decrease</button>
 //         </div>
 //     )
 // }
